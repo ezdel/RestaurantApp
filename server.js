8a19b62a9a3d68844beb59101b8171aa10f66360
iniezen <iniezen@gmail.com>
2016-09-24 14:19:24 -0700

    post and get requests are working

--- server.js
+++ server.js
@@ -24,12 +24,23 @@ var customers = [];
 
 // Basic route that sends the user first to the AJAX Page
 app.get('/', function (req, res) {
-	res.sendFile(path.join(__dirname, 'tables.html'));
+	res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.get('/add', function (req, res) {
-	res.sendFile(path.join(__dirname, 'reservations.html'));
+app.get('/reservations', function (req, res) {
+	res.sendFile(path.join(__dirname, 'html/reservations.html'));
 });
+
+app.get('/api/tables', function (req, res) {
+	res.send(customers);
+});
+
+// Create New customers - takes in JSON input
+app.post('/api/new', function (req, res) {
+	customers.push(req.body);
+});
+
+
 
 // Search for Specific customer (or all customers) - provides JSON
 app.get('/api/:customers?', function (req, res) {
