5f0dc5da62425b8cfd475ca1aec7a47d15309bda
Aaron A <albon101@Aarons-MacBook-Pro.local>
2016-09-24 13:28:48 -0700

    added query.js

---
+++ query.js
@@ -0,0 +1,51 @@
+function available() {
+	var URL = window.location.origin;
+	$.ajax({url: URL + "/api/tables", method: "GET"})
+		.done(function(tableData) {
+			for(var i = 0; i < tableView.length; i++) {
+				var tableView = $("<div>");
+				tableView.addClass('holder');
+				tableView.attr('id', 'placeholder-' + i+1)
+				$('#tableView').append(tableView);
+
+				var resNumber = i + 1;
+				$("#tableWell-"+ i+1).append('<h2><span class="label label-primary">' + tableNumber + "</span> | " + tableData[i].customerID + "</h2>");
+			}
+				
+		})
+}
+
+function waitList() {
+	var URL = window.location.origin;
+	$.ajax({url: URL + "/api/waitList", method: "GET"})
+		.done(function(waitListData) {
+			for (var i = 0; i < waitListData.length; i++) {
+				var waitList = $("<div>");
+				waitList.addClass("holder");
+				waitList.attr('id', 'placeholder' + i+1)
+				$('#waitlistSection').append(waitList);
+
+				var resNumber = i + 1;
+				$("#tableSection"+ i+1).append('<h2><span class="label label-primary">' + tableNumber + "</span> | " + waitlistData[i].customerID + "</h2>");
+			}
+		})
+}
+
+function clearTable(){
+	var URL = window.location.origin;
+	$.ajax({url: URL + "/api/clear", method: "POST"})
+}
+
+$("#clear").on('click', function() {
+	alert("Get Out")
+	clearTable();
+	location.reload();
+})
+
+
+available();
+waitList();
+
+
+
+
